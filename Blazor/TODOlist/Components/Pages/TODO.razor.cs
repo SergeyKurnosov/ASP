8851bd8c1952055c56ef32875240aa0b6576d81f
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-03-18 23:00:53 +1000

    todo list almost done

---
+++ Blazor/TODOlist/Components/Pages/TODO.razor.cs
@@ -0,0 +1,19 @@
+﻿using TODOlist.Classes;
+
+namespace TODOlist.Components.Pages
+{
+	public partial class TODO
+	{
+		string task = "Hello";
+
+		HashSet<TODOitem> todos = [];
+
+		void AddTask()
+		{
+			if (string.IsNullOrWhiteSpace(task)) return;
+			if (todos.Contains(new TODOitem { Title = task })) return;
+			todos.Add(new TODOitem { Title = task });
+			task = "";
+		}
+	}
+}
