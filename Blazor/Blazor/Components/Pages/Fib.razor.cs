76bf715e69d2c84b5dcdaa0aefb0f742e0713354
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-03-18 20:28:29 +1000

    hw 16_03_2026 done

---
+++ Blazor/Blazor/Components/Pages/Fib.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Fib
+	{
+		int number;
+		List<BigInteger> results = new List<BigInteger>();
+
+		void setNumber(int number)
+		{
+			this.number = number;
+		}
+		void Calculate()
+		{
+			results = new List<BigInteger> {0,1};
+
+			for (int i = 2; i < number; i++)
+			{
+				BigInteger result = results[i-1] + results[i-2];
+				results.Add(result);
+			}
+		}
+	}
+}
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		int number;
+		int exponent;
+
+		BigInteger result = 1;
+		void setNumber(int number)
+		{
+			this.number = number;
+		}
+		void Calculate()
+		{
+			result = (BigInteger)Math.Pow(this.number, this.exponent);
+		}
+	}
+}
