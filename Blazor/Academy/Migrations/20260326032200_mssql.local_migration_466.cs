722567a15bb83187810f7b5af4a2554849ee0392
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-03-26 13:31:00 +1000

    class work ispravleno

---
+++ Blazor/Academy/Migrations/20260326032200_mssql.local_migration_466.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academy.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_466 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Directions",
+                columns: table => new
+                {
+                    direction_id = table.Column<byte>(type: "tinyint", nullable: false),
+                    direction_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directions", x => x.direction_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Directions");
+        }
+    }
+}
