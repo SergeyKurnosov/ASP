f9a52103b3225df0bb55779a980310252e268c12
SergeyKurnosov <kurnosovsergej123@mail.ru>
2026-03-30 21:40:57 +1000

    Unique directionNameAttribute Nika

---
+++ Blazor/Academy/Migrations/20260328021121_mssql.local_migration_370.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Academy.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_370 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "direction_name",
+                table: "Directions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Directions_direction_name",
+                table: "Directions",
+                column: "direction_name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Directions_direction_name",
+                table: "Directions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "direction_name",
+                table: "Directions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
